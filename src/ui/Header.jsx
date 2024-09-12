58a19c38bbb864f3780fa400207c035add35124c
Ivan Zapanta <ivanzapanta@gmail.com>
2024-09-12 00:10:44 -0400

    added AppLayout as the parent route

---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Xtreme Pizza</Link>
+
+      <p>Ivan</p>
+    </header>
+  );
+}
+
+export default Header;
